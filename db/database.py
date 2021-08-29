f9853c8225e321e48c8302926270feb2e170685e
blacha370 <lukasz.blacha370@gmail.com>
2021-08-29 18:14:25 +0200

    [TASK-1] Create db module.

---
+++ db/database.py
@@ -0,0 +1,13 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@postgres:5432/postgres"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
