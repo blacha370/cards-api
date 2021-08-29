62d32dfbd503a1f6ce531275a4d5b762fd569922
blacha370 <lukasz.blacha370@gmail.com>
2021-08-29 18:16:05 +0200

    [TASK-1] Add db and auth to main.py.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
+from backend.auth.models import Base
+from backend.auth.views import router as auth_router
+from backend.db.database import engine
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.include_router(auth_router, prefix="/auth")
 
 
 @app.get("/")
